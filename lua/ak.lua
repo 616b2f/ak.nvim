fb7857391eadfb3d7a1dc84eb56b3c22e550d884
Alexej Kowalew <616b2f@gmail.com>
2025-04-17 21:15:48 +0200

    feat: add code_search

--- lua/ak.lua
+++ lua/ak.lua
@@ -163,6 +163,55 @@ M.ui.jwt.decode = function(command_args)
   M.ui.replace_selection(jsons)
 end
 
+--- Search inside files with extended syntax (in future)
+M.ui.code_search = function(opts)
+  -- initial code was copied from https://github.com/tjdevries/advent-of-nvim (thanks TJ)
+  local pickers = require "telescope.pickers"
+  local finders = require "telescope.finders"
+  local make_entry = require "telescope.make_entry"
+  local conf = require "telescope.config".values
+
+  opts = opts or {}
+  opts.cwd = opts.cwd or vim.uv.cwd()
+
+  local finder = finders.new_async_job {
+    command_generator = function(prompt)
+      if not prompt or prompt == "" then
+        return nil
+      end
+
+      local pieces = vim.split(prompt, "  ")
+      local args = { "rg" }
+      if pieces[1] then
+        table.insert(args, "-e")
+        table.insert(args, pieces[1])
+      end
+
+      -- TODO: add possibility to search with `path:` and `project:` prefixes
+      if pieces[2] then
+        table.insert(args, "-g")
+        table.insert(args, pieces[2])
+      end
+
+      return vim.iter({
+          args,
+          { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" },
+        })
+        :flatten()
+        :totable()
+    end,
+    entry_maker = make_entry.gen_from_vimgrep(opts),
+    cwd = opts.cwd,
+  }
+
+  pickers.new(opts, {
+    debounce = 100,
+    prompt_title = "Code Search",
+    finder = finder,
+    previewer = conf.grep_previewer(opts),
+    sorter = require("telescope.sorters").empty(),
+  }):find()
+end
 
 M.fs = {}
 
